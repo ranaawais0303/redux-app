e1f0f77b5ceddf411616be9f09e5f3ab93f3663f
Rana_awais <z.manzoor@pioneerlogics.com>
2022-07-17 23:24:15 -0700

    boot strap nav bar and shop component

---
+++ src/Component/Shop.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Shop(props) {
+  return (
+    <div>
+      <h2>Buy adidas</h2>
+      <button className="btn btn-primary">-</button>
+      Add to cart
+      <button className="btn btn-primary">+</button>
+    </div>
+  );
+}
+export default Shop;
